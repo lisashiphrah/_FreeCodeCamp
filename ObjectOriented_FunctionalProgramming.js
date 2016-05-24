d768512227c98b8f60db9bf578fc79b9a0639eb6
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-05-24 17:15:36 -0300

    +Finished script "Declare JavaScript Objects as Variables"

---
+++ ObjectOriented_FunctionalProgramming.js
@@ -0,0 +1,10 @@
+//Declare JavaScript Objects as Variables
+var motorBike = {
+  "wheels":2,
+  "engines":1,
+  "seats":2
+  // Only change code below this line.
+};
+
+
+//Construct JavaScript Objects with Functions
