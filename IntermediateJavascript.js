759d8af16ff7f97092a6d503b35c92e987577f70
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-07-25 11:35:12 -0300

    added functions

--- IntermediateJavascript.js
+++ IntermediateJavascript.js
@@ -1,4 +1,5 @@
 
+//Sum all
 function sumAll(arr) {
   var max = Math.max(arr[0], arr[1]);
   var min = Math.min(arr[0], arr[1]);
@@ -16,3 +17,52 @@ function sumAll(arr) {
 sumAll([1, 4]);
 
 
+
+//Diff Two Arrays
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  var element = 0;
+ 
+  for(var index = 0; index < arr1.length; index++)
+    {
+      element = arr1[index];
+      if(arr2.indexOf(element) == -1)
+        {
+          newArr.push(element);
+        }
+    }
+  
+  for(var index2 = 0; index2 < arr2.length; index2++)
+    {
+      element = arr2[index2];
+      if(arr1.indexOf(element) == -1)
+        {
+          newArr.push(element);
+        }
+    }
+  
+  
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+//Roman Numeral Converter
+function convertToRoman(num) {
+  var decimalArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romanArray = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  var romanConverted = [];
+  
+  for(var index = 0; index < decimalArray.length; index++)
+    {
+      while(decimalArray[index] <= num)
+        {
+          num = num - decimalArray[index];
+          romanConverted.push(romanArray[index]);
+        }
+    }
+ return romanConverted.join('');
+}
+
+convertToRoman(36);
