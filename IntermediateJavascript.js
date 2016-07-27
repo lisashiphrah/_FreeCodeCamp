56a0e66c0864cca60fa0f6382570d2558ad10715
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-07-27 10:38:13 -0300

    + Search and Replace function

--- IntermediateJavascript.js
+++ IntermediateJavascript.js
@@ -1,19 +1,14 @@
-
 //Sum all
 function sumAll(arr) {
   var max = Math.max(arr[0], arr[1]);
   var min = Math.min(arr[0], arr[1]);
-  
-  var result = 0;
-  
+  var result = 0; 
   for(var i = min; i <= max; i++)
     {
       result = result + i;
     }
-  
   return result;
 }
-
 sumAll([1, 4]);
 
 
@@ -22,7 +17,6 @@ sumAll([1, 4]);
 function diffArray(arr1, arr2) {
   var newArr = [];
   var element = 0;
- 
   for(var index = 0; index < arr1.length; index++)
     {
       element = arr1[index];
@@ -31,7 +25,6 @@ function diffArray(arr1, arr2) {
           newArr.push(element);
         }
     }
-  
   for(var index2 = 0; index2 < arr2.length; index2++)
     {
       element = arr2[index2];
@@ -42,7 +35,6 @@ function diffArray(arr1, arr2) {
     }
   return newArr;
 }
-
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
 
@@ -51,7 +43,6 @@ function convertToRoman(num) {
   var decimalArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   var romanArray = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
   var romanConverted = [];
-  
   for(var index = 0; index < decimalArray.length; index++)
     {
       while(decimalArray[index] <= num)
@@ -62,7 +53,6 @@ function convertToRoman(num) {
     }
  return romanConverted.join('');
 }
-
 convertToRoman(36);
 
 
@@ -73,8 +63,7 @@ function whatIsInAName(collection, source) {
     {
         var obj = collection[index];
         var found = true;
-      
-        for(var prop in source)
+      	for(var prop in source)
         {
           if(source[prop] != obj[prop])
             {
@@ -86,9 +75,23 @@ function whatIsInAName(collection, source) {
             arr.push(collection[index]);
         }
     }
-  // Only change code above this line
   return arr;
 }
 
 
 //Search and Replace
+function myReplace(str, before, after) {
+  if(before[0].toUpperCase() == before[0])
+    {
+      var arr = after.split('');
+      arr[0] = arr[0].toUpperCase();
+      after = arr.join('');
+    }
+  var newString = str.replace(before, after); 
+  return newString;
+}
+myReplace("His name is Tom", "Tom", "john");
+
+
+
+//Pig Latin
