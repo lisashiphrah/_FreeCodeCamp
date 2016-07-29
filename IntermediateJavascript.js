0d2c3730f29bf72f3b0f1d3dba3dfacdcc0c8861
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-07-29 16:44:26 -0300

    + Finders Keepers function

--- IntermediateJavascript.js
+++ IntermediateJavascript.js
@@ -304,3 +304,12 @@ function smallestCommons(arr) {
  
   return quot;
 }
+
+
+//Finders Keepers
+function findElement(arr, func) {
+  var newArray = arr.filter(func);
+  return newArray[0];
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
