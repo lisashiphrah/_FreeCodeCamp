185b647e4257e14991ab5609e8dc370dc7414e64
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-08-02 15:38:50 -0300

    +Everything Be True function

--- IntermediateJavascript.js
+++ IntermediateJavascript.js
@@ -360,3 +360,19 @@ function binaryAgent(str) {
   return result.join('');
 }
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+
+
+//Everything Be True
+function truthCheck(collection, pre) {
+  for(var index = 0; index < collection.length; index++)
+    {
+      var element = collection[index];
+    
+      if(!element[pre])
+        {
+          return false;
+        }
+    }
+  return true;
+}
