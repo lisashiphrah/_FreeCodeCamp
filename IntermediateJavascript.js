8ccfcd5f5f3fa725a6c16af56244e49a72dcda0c
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-08-02 16:05:04 -0300

    Finished all Intermediate Javascript functions module

--- IntermediateJavascript.js
+++ IntermediateJavascript.js
@@ -376,3 +376,33 @@ function truthCheck(collection, pre) {
     }
   return true;
 }
+
+
+//Algorithm Arguments Optional
+function addTogether() {
+  var args = new Array(arguments.length);
+  for(var i = 0; i < args.length; ++i) {
+      args[i] = arguments[i];
+    }
+ if(args.length == 2){
+    if(typeof args[0] !== 'number' || typeof args[1] !=='number' ){
+      return undefined;
+      }
+    return args[0]+args[1];
+   }
+ if(args.length == 1){
+     a= args[0];
+    if(typeof a!=='number'){
+        return undefined;
+      }
+    else{
+       return function(b){
+         if(typeof b !=='number'){
+           return undefined;
+           }
+         else
+           return a+b;
+          };
+      }
+    }
+}
