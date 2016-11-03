569d50aadcd229c7882167d7b55cdc4ad87243ff
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-11-03 16:56:50 +0000

    Exact Change method changes

--- AdvancedJavascript.js
+++ AdvancedJavascript.js
@@ -29,3 +29,21 @@ sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5])
 
 
 //Exact Change
+function checkCashRegister(price, cash, cid) {
+  var change;
+  // Here is your change, ma'am.
+  return change;
+}
+
+// Example cash-in-drawer array:
+// [["PENNY", 1.01],
+// ["NICKEL", 2.05],
+// ["DIME", 3.10],
+// ["QUARTER", 4.25],
+// ["ONE", 90.00],
+// ["FIVE", 55.00],
+// ["TEN", 20.00],
+// ["TWENTY", 60.00],
+// ["ONE HUNDRED", 100.00]]
+
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
