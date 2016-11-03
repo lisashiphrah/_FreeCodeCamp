1a2d77af6d8f9db3aecc48a866694042291af08d
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-11-03 17:05:47 +0000

    Finished function exact change

--- AdvancedJavascript.js
+++ AdvancedJavascript.js
@@ -29,10 +29,78 @@ sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5])
 
 
 //Exact Change
+
 function checkCashRegister(price, cash, cid) {
-  var change;
-  // Here is your change, ma'am.
-  return change;
+    cash = cash * 100;
+    price = price * 100;
+ 
+    var change = cash - price;
+    var changeLeft = change;
+ 
+    var totalCid = getTotalCid(cid);
+    var result =[];
+ 
+    if (change > totalCid) {
+        return 'Insufficient Funds';
+    } else if (change === totalCid) {
+        return 'Closed';
+    }
+ 
+    for (var i = cid.length - 1; i >= 0; i--) {
+        var coinName = cid[i][0],
+            coinTotal = cid[i][1] * 100,
+            coinValue = getValue(coinName),
+            coinAmount = coinTotal / coinValue,
+            toReturn = 0;
+ 
+        while (changeLeft >= coinValue && coinAmount > 0) {
+            changeLeft -= coinValue;            
+            coinAmount--;
+            toReturn++;
+            console.log(changeLeft);
+        }
+ 
+        
+        if (toReturn > 0) {
+            result.push([coinName, toReturn * (coinValue / 100)]);
+        }
+    }
+ 
+    if (getTotalCid(result) != change) {
+        return 'Insufficient Funds';           
+    }
+ 
+    return result; 
+ 
+ 
+    function getTotalCid(cid) {
+        var total = 0;
+        
+        return total;
+    }
+ 
+    function getValue(coinOrBill) {
+        switch (coinOrBill) {
+            case 'PENNY':
+                return 1;
+            case 'NICKEL':
+                return 5;
+            case 'DIME':
+                return 10;
+            case 'QUARTER':
+                return 25;
+            case 'ONE':
+                return 100;
+            case 'FIVE':
+                return 500;
+            case 'TEN':
+                return 1000;
+            case 'TWENTY':
+                return 2000;
+            case 'ONE HUNDRED':
+                return 10000;
+        }
+    }
 }
 
 // Example cash-in-drawer array:
