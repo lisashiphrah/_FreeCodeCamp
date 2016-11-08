aa23a54007068328b5b5ad1bfafba8b1f5f1545a
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-11-08 14:53:40 +0000

    [Map the Debris] - ended

--- AdvancedJavascript.js
+++ AdvancedJavascript.js
@@ -333,8 +333,33 @@ var Person = function(firstAndLast) {
   };
   
 };
-
 var bob = new Person('Bob Ross');
 bob.getFullName();
+
+
+//Map the Debris
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  var a = 2 * Math.PI;
+  var newArr = [];
+  
+  var getOrbPeriod = function(obj) {
+    var c = Math.pow(earthRadius + obj.avgAlt, 3);
+    var b = Math.sqrt(c / GM);
+    var orbPeriod = Math.round(a * b);
+    delete obj.avgAlt;
+    obj.orbitalPeriod = orbPeriod;
+    return obj;
+  };
+
+  for (var elem in arr) {
+    newArr.push(getOrbPeriod(arr[elem]));
+  }
+
+  return newArr;
+}
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
 
 
