56c581d2dfb13ce3cefee2cdeb73016c2e118179
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-11-08 14:46:39 +0000

    [Make a Person] - done

--- AdvancedJavascript.js
+++ AdvancedJavascript.js
@@ -300,7 +300,41 @@ function makeFriendlyDates(arr) {
   
   return result;
 }
-
 makeFriendlyDates(["2022-09-05", "2023-09-04"]);
+
+
+//Make a Person
+var Person = function(firstAndLast) {
+
+  var fullName = firstAndLast.split(' ');
+  
+  this.getFirstName = function() {
+    return fullName[0];
+  };
+  
+  this.getLastName = function() {
+    return fullName[1];
+  };
+  
+  this.getFullName = function() {
+    return fullName.join(' ');
+  };
+  
+  this.setFirstName = function(first) {
+    fullName[0] = first;
+  };
+  
+  this.setLastName = function(last) {
+    fullName[1] = last;
+  };
+  
+  this.setFullName = function(firstAndLast) {
+    fullName = firstAndLast.split(' ');
+  };
+  
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
 
 
