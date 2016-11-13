653a631c7149a365b84cdfe0be6450aff632b150
Cassia Almeida <lisa.shiphrah@gmail.com>
2016-11-13 18:42:00 +0000

    Update IntermediateJavascript.js

--- IntermediateJavascript.js
+++ IntermediateJavascript.js
@@ -3,8 +3,7 @@ function sumAll(arr) {
   var max = Math.max(arr[0], arr[1]);
   var min = Math.min(arr[0], arr[1]);
   var result = 0; 
-  for(var i = min; i <= max; i++)
-    {
+  for(var i = min; i <= max; i++) {
       result = result + i;
     }
   return result;
