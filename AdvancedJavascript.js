d013a5a393a5ec9863d26f86adb15302a30272a6
Cassia Almeida <lisa.shiphrah@gmail.com>
2016-11-13 18:41:25 +0000

    Update AdvancedJavascript.js

--- AdvancedJavascript.js
+++ AdvancedJavascript.js
@@ -1,4 +1,4 @@
-//Validate US Telephone Numbers
+//Validate US Telephone Numbers 
 function telephoneCheck(str) {
   var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
   return regex.test(str);
