575394b94cf286907cdee20270906ec141c295dd
Cassia Almeida <lisa.shiphrah@gmail.com>
2018-08-30 10:48:04 +0100

    Added Use Multiple Conditional (Ternary) Operators

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -144,3 +144,13 @@ function checkEqual(a, b) {
 }
 
 checkEqual(1, 2);
+
+
+///////////////////////////////////////////////////////////////////
+//Use Multiple Conditional (Ternary) Operators
+///////////////////////////////////////////////////////////////////
+function checkSign(num) {
+  return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero";
+}
+
+checkSign(10);
